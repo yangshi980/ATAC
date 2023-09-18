9ba9d4b23c43be986d877df4573226d0588b4c80
yangshi <yangshi980>
2023-09-18 17:49:40 +0800

    add re-install pytorch

--- extra.sh
+++ extra.sh
@@ -1,2 +1,3 @@
 pip install "cython<3"
-conda install -c anaconda libstdcxx-ng=13
+conda install -c "conda-forge/label/gcc7" libstdcxx-ng
+conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 -c pytorch --force-reinstall
